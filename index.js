85c8fd161d67dc6130bd20b21e98c8de5efd99a4
1NJ57C3 <85198277+1NJ57C3@users.noreply.github.com>
2021-11-22 15:15:07 -0800

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
 // Write your code in this file!
+const currentUser = '1NJ57C3';
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser}!`;
+// const excitedWelcomeMessage = `${welcomeMessage.toUpperCase}`;
+const shortGreeting = `Welcome, ${currentUser.slice(0,1)}!`;
